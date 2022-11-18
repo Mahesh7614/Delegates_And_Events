dc529f235583e5aeace66cfd902f74cfb06bb1ab
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-18 21:56:33 +0530

    Event Handling

--- Delegates_And_Events/Program.cs
+++ Delegates_And_Events/Program.cs
@@ -5,11 +5,14 @@
         public static event DelEventHandler Del;
         static void Main(string[] args)
         {
+            Console.WriteLine("******************** Event Handling *****************************");
+            EventHandling.ImplementEvent();
+
             Del += new DelEventHandler(EventProblem.India);
             Del += new DelEventHandler(EventProblem.USA);
             Del += new DelEventHandler(EventProblem.England);
             Del.Invoke();
-            
+
             ArrayDelegates.ImplementDelegates();
         }
     }
