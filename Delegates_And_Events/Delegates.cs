d2b3406b427d59f972de5435dbddba3a1a685083
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-18 21:38:40 +0530

    Single Cast Delegate

---
+++ Delegates_And_Events/Delegates.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace Delegates_And_Events
+{
+    public class Delegates
+    {
+        public static void Addition(int a, int b)
+        {
+            Console.WriteLine("Addition is : " + (a + b));
+        }
+        public static void Substraction(int a, int b)
+        {
+            Console.WriteLine("Substraction is : " + (a - b));
+        }
+        public static void Multiplication(int a, int b)
+        {
+            Console.WriteLine("Multiplication is : " + (a * b));
+        }
+    }
+    public class ArrayDelegates
+    {
+        delegate void Del(int x, int y);
+
+        public static void ImplementDelegates()
+        {
+            Del[] obj =
+            {
+                new Del(Delegates.Addition),
+                new Del(Delegates.Multiplication),
+                new Del(Delegates.Substraction),
+            };
+
+            for (int i =0; i < obj.Length; i++)
+            {
+                obj[i](10, 5);
+                obj[i](20, 10);
+                obj[i](40, 20);
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Delegates_And_Events/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Delegates_And_Events
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayDelegates.ImplementDelegates();
+        }
+    }
+}
